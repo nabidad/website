46263dd334054757bba660ff1d728a6b09c27984
nabidad <nabidadkhan.center@gmail.com>
2024-05-04 12:33:42 +0500

    Created Sales Page and JS

---
+++ sales.js
@@ -0,0 +1,261 @@
+// First Card
+function changeImage(x,image)
+{
+if(x==1)
+{
+image.src = '/images/cards/img 1(a).jpg';    
+}
+if(x == 2)
+{
+image.src = '/images/cards/img 1.jpg';
+}
+}
+
+// Card 2
+function changeImage2(x,image)
+{
+if(x==1)
+{
+image.src = '/images/cards/img 2(a).jpg';    
+}
+if(x == 2)
+{
+image.src = '/images/cards/img 2.jpg';
+}
+}
+
+// Card 3
+function changeImage3(x,image)
+{
+if(x==1)
+{
+image.src = '/images/cards/img 3.jpg';    
+}
+if(x == 2)
+{
+image.src = '/images/cards/img 3(a).jpg';
+}
+}
+
+// Card 4
+function changeImage4(x,image)
+{
+if(x==1)
+{
+image.src = '/images/cards/img 4.jpg';    
+}
+if(x == 2)
+{
+image.src = '/images/cards/img 4(a).jpg';
+}
+}
+
+// Second Line
+// Card 5
+function changeImage5(x,image)
+{
+if(x==1)
+{
+image.src = '/images/cards/img 5.jpg';    
+}
+if(x == 2)
+{
+image.src = '/images/cards/img 5(a).jpg';
+}
+}
+
+// Card 6
+function changeImage6(x,image)
+{
+if(x==1)
+{
+image.src = '/images/cards/img 6.jpg';    
+}
+if(x == 2)
+{
+image.src = '/images/cards/img 6(a).jpg';
+}
+}
+
+// Card 7
+function changeImage7(x,image)
+{
+if(x==1)
+{
+image.src = '/images/cards/img 7.jpg';    
+}
+if(x == 2)
+{
+image.src = '/images/cards/img 7(a).jpg';
+}
+}
+
+// Card 8
+function changeImage8(x,image)
+{
+if(x==1)
+{
+image.src = '/images/cards/img 8.jpg';    
+}
+if(x == 2)
+{
+image.src = '/images/cards/img 8(a).jpg';
+}
+}
+
+// Third Line
+// Card 9
+function changeImage9(x,image)
+{
+if(x==1)
+{
+image.src = '/images/cards/img 9.jpg';    
+}
+if(x == 2)
+{
+image.src = '/images/cards/img 9(a).jpg';
+}
+}
+
+// Card 10
+function changeImage10(x,image)
+{
+if(x==1)
+{
+image.src = '/images/cards/img 10.jpg';    
+}
+if(x == 2)
+{
+image.src = '/images/cards/img 10(a).jpg';
+}
+}
+
+// Card 11
+function changeImage11(x,image)
+{
+if(x==1)
+{
+image.src = '/images/cards/img 11.jpg';    
+}
+if(x == 2)
+{
+image.src = '/images/cards/img 11(a).jpg';
+}
+}
+
+// Card 12
+function changeImage12(x,image)
+{
+if(x==1)
+{
+image.src = '/images/cards/img 12.jpg';    
+}
+if(x == 2)
+{
+image.src = '/images/cards/img 12(a).jpg';
+}
+}
+
+
+// Fourth Line
+// Card 13
+function changeImage13(x,image)
+{
+if(x==1)
+{
+image.src = '/images/cards/img 13.jpg';    
+}
+if(x == 2)
+{
+image.src = '/images/cards/img 13(a).jpg';
+}
+}
+
+// Card 14
+function changeImage14(x,image)
+{
+if(x==1)
+{
+image.src = '/images/cards/img 14.jpg';    
+}
+if(x == 2)
+{
+image.src = '/images/cards/img 14(a).jpg';
+}
+}
+
+// Card 15
+function changeImage15(x,image)
+{
+if(x==1)
+{
+image.src = '/images/cards/img 15.jpg';    
+}
+if(x == 2)
+{
+image.src = '/images/cards/img 15(a).jpg';
+}
+}
+
+// Card 16
+function changeImage16(x,image)
+{
+if(x==1)
+{
+image.src = '/images/cards/img 16.jpg';    
+}
+if(x == 2)
+{
+image.src = '/images/cards/img 16(a).jpg';
+}
+}
+
+// Fifth Line
+// Card 17
+function changeImage17(x,image)
+{
+if(x==1)
+{
+image.src = '/images/cards/img 17.jpg';    
+}
+if(x == 2)
+{
+image.src = '/images/cards/img 17(a).jpg';
+}
+}
+// Card 13
+function changeImage18(x,image)
+{
+if(x==1)
+{
+image.src = '/images/cards/img 18.jpg';    
+}
+if(x == 2)
+{
+image.src = '/images/cards/img 18(a).jpg';
+}
+}
+// Card 13
+function changeImage19(x,image)
+{
+if(x==1)
+{
+image.src = '/images/cards/img 19.jpg';    
+}
+if(x == 2)
+{
+image.src = '/images/cards/img 19(a).jpg';
+}
+}
+// Card 13
+function changeImage20(x,image)
+{
+if(x==1)
+{
+image.src = '/images/cards/img 20.jpg';    
+}
+if(x == 2)
+{
+image.src = '/images/cards/img 20(a).jpg';
+}
+}
